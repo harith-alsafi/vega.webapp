2282edd1296fc5e12ca645e5ecaac2083da91d11
unknown <Harith Al-Safi>
2024-01-23 17:09:39 +0000

    Checked if we can execute two functions after one another and we cann!!

--- src/components/chat-list.tsx
+++ src/components/chat-list.tsx
@@ -8,6 +8,23 @@ export interface ChatList {
   messages: Message[]
 }
 
+export function SingleChat({message, index, maxLength}: {message: Message, index: number, maxLength: number}){
+  if(message.role === 'function' && message.name == "plot-data"){
+    return <div key={index}> <ChartMessage/> </div>
+  }
+  else if(message.role === 'function' && message.name == "get_current_weather"){
+    return <div key={index}> <h1> Weather </h1> </div>
+  }
+  else if((message.role === 'assistant' && message.function_call == null )|| message.role === "user"){
+    return  <div key={index}>
+      <ChatMessage message={message} />
+    {index < maxLength - 1 && (
+      <Separator className="my-4 md:my-8" />
+    )}
+    </div>
+  }
+}
+
 export function ChatList({ messages }: ChatList) {
   if (!messages.length) {
     return null
@@ -15,21 +32,22 @@ export function ChatList({ messages }: ChatList) {
   return (
     <div className="relative mx-auto max-w-2xl px-4">
       {messages.map((message, index) => {
-        if(message.role === 'function'){
-          // console.log(message)
-        }
-        return (message.role === 'assistant' && message.function_call == null )|| message.role === "user" ? (
-          <div key={index}>
-          <ChatMessage message={message} />
-          {index < messages.length - 1 && (
-            <Separator className="my-4 md:my-8" />
-          )}
-        </div>
-        ) : (
-        message.role === 'function' && message.name == "plot-data"?
-        <div key={index}>
-          <ChartMessage/>
-        </div> : null)
+        return <SingleChat key={index} message={message} index={index} maxLength={messages.length}/>
+        // if(message.role === 'function'){
+        //   // console.log(message)
+        // }
+        // return (message.role === 'assistant' && message.function_call == null )|| message.role === "user" ? (
+        //   <div key={index}>
+        //   <ChatMessage message={message} />
+        //   {index < messages.length - 1 && (
+        //     <Separator className="my-4 md:my-8" />
+        //   )}
+        // </div>
+        // ) : (
+        // message.role === 'function' && message.name == "plot-data"?
+        // <div key={index}>
+        //   <ChartMessage/>
+        // </div> : null)
       })}
     </div>
   )
